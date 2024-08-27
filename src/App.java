0163061c2b5f2b523cfc13c0596be5c9c106a347
Jason Owen <jasonowen101@gmail.com>
2024-08-27 11:52:31 -0500

    JTO Removing Sample Name

--- src/App.java
+++ src/App.java
@@ -9,7 +9,6 @@ public class App {
 
         // Add Student objects to the list
         students.add(new Student("Jason", "Owen", "CS"));
-        students.add(new Student("Susan", "Stewart", "CIS"));
         students.add(new Student("Mark", "Sharovarov", "CS"));
         students.add(new Student("Ethan", "Sexton", "CS"));
 
