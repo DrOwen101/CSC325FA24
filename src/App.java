100a9fb8297d4ec28e930fc25701da51415e9b13
squirrelyCoding <georgeabbie0@gmail.com>
2024-08-28 14:02:40 -0500

    AG

--- src/App.java
+++ src/App.java
@@ -12,7 +12,7 @@ public class App {
         students.add(new Student("Mark", "Sharovarov", "CS"));
         students.add(new Student("Ethan", "Sexton", "CS"));
         students.add(new Student("William", "Ashford", "CS"));
-        // hold
+        students.add(new Student("Abbie", "George", "CS"));
 
         // Loop through the list and output information about each student
         for (Student student : students) {
