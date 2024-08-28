41137100f0e9db546ee6ed888aedc365de7b1925
Kaleb Gore <91081805+KalebGore28@users.noreply.github.com>
2024-08-28 09:39:52 -0500

    initial push

--- src/App.java
+++ src/App.java
@@ -9,8 +9,8 @@ public class App {
 
         // Add Student objects to the list
         students.add(new Student("Jason", "Owen", "CS"));
-        students.add(new Student("Mark", "Sharovarov", "CS"));
-        students.add(new Student("Ethan", "Sexton", "CS"));
+        students.add(new Student("Susan", "Sharovarov", "CS"));
+        students.add(new Student("Kaleb", "Gore", "CS"));
 
         // Loop through the list and output information about each student
         for (Student student : students) {
