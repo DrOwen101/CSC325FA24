7ffb7daf6f4a57077f232ee68b253232fc9199bd
emiliakubik <ekubik1@murraystate.edu>
2024-08-30 09:07:04 -0500

    JTO Test Sync

---
+++ EmiliaKubik_Animal/Cat.java
@@ -0,0 +1,9 @@
+public class Cat extends Animal {
+    public void makeSound(){
+        System.out.println("The cat meows.");
+    }
+
+    public void move(){
+        System.out.println("The cat moves on four legs.");
+    }
+}
