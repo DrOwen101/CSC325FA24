ffefd9d98fc2c5bd4a77000f643e1c1cd227abc3
Unluckytoast <161383544+Unluckytoast@users.noreply.github.com>
2024-08-30 11:06:20 -0500

    Add files via upload

---
+++ JacksonBradley_Animal/main.java
@@ -0,0 +1,15 @@
+
+
+public class main {
+public static void main(String[] args){
+Dog myDog = new Dog();
+Cat myCat = new Cat();
+myDog.sound();
+myDog.move();
+
+myCat.sound();
+myCat.move();
+}
+}
+ 
+
