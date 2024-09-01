9d380fc1118104dd70a18ff0b67db44aa55fe3b8
JacksonCash <jackson.w.cash04@gmail.com>
2024-08-31 21:50:40 -0500

    JacksonCash_Animal Project I've saved, commited, and synced my branch.

---
+++ src/JacksonCash_Animal/Animal.java
@@ -0,0 +1,30 @@
+package JacksonCash_Animal;
+
+abstract class Animal {
+    abstract String sound();
+    abstract String moving();
+}
+class Dog extends Animal {
+    String sound() {
+        return "\"Woof!\"";
+    }
+    String moving() {
+        return "four";
+    }
+}
+class Cat extends Animal {
+    String sound() {
+        return "\"Meow!\"";
+    }
+    String moving() {
+        return "four";
+    }
+}
+class Main {
+    public static void main(String[] args) {
+        Animal dog=new Dog();
+        Animal cat=new Cat();
+        System.out.println("A dog says " + dog.sound() + " and moves on " + dog.moving() + " legs.");
+        System.out.println("A cat says " + cat.sound() + " and moves on " + cat.moving() + " legs.");
+    }
+}
