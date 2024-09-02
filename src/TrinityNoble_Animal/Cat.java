d34187dcf2e597959ccc4f3a39f7bc1a150cf94d
trinitynoble <trinitycnoble@gmail.com>
2024-09-01 20:50:25 -0500

    Sync before pull request

---
+++ src/TrinityNoble_Animal/Cat.java
@@ -0,0 +1,11 @@
+package TrinityNoble_Animal;
+//Done by: Trinity Noble
+//01.09.24
+//CS325-01-FA24
+class Cat extends Animal {
+    //Printing out the specific info pertaining to the method.
+    void sound(){System.out.println("A cat goes 'Meow!', if they're not hissing at you.");}
+
+    void num_legs(){System.out.println("Cats normally have 4 legs.");}
+}
+
---
+++ src/TrinityNoble_Animal/Main.java
@@ -0,0 +1,18 @@
+package TrinityNoble_Animal;
+//Done by: Trinity Noble
+//01.09.24
+//CS325-01-FA24
+public class Main {
+    public static void main(String[] args) {
+        //creating instances of both cat and dog to run the methods.
+        Animal d = new Dog();
+        Animal c = new Cat();
+        //printing the information gathered by the methods
+        c.sound();
+        c.num_legs();
+        //////////////////////////////////////////
+        System.out.println(" ");
+        d.sound();
+        d.num_legs();
+    }
+}
