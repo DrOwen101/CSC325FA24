5c4ea280d835cb3584a4991041c2b749a620419c
trinitynoble <trinitycnoble@gmail.com>
2024-09-06 08:37:49 -0500

    ...

--- src/TrinityNoble_Animal/Animal.java
+++ src/TrinityNoble_Animal/Animal.java
@@ -1,7 +1,7 @@
 package TrinityNoble_Animal;
 //Done by: Trinity Noble
 //01.09.24
-//CS325-01-FA24
+//CS325-01-FA24 
 abstract class Animal {
 //creating the methods for the animal abstract class
     abstract void sound();
