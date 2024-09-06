abbbe61baaeb607ed520331ca9953e54769a452b
Jason Owen <jasonowen101@gmail.com>
2024-09-06 14:58:22 -0500

    JTO Fixed Main with pushed car classes as they sit.

---
+++ src/as2_2/Assignment2/src/JohnEnglishCar.java
@@ -0,0 +1,26 @@
+public class JohnEnglishCar {
+
+    public String carType;
+    public String carColor;
+    public int carYear;
+
+    public void Car(String type, String color, int year) {
+        carType = type;
+        carColor = color;
+        carYear = year;
+    }
+
+    public void Car(){}
+
+    // Getters
+    public String getType() {return carType;}
+    public String getColor() {return carColor;}
+    public int getYear() {return carYear;}
+
+    // Setters
+    public void setType(String type) {carType = type;}
+    public void setColor(String color) {carColor = color;}
+    public void setYear(int year) {carYear = year;}
+
+}
+
--- src/as2_2/Assignment2/src/MainApp.java
+++ src/as2_2/Assignment2/src/MainApp.java
@@ -2,5 +2,10 @@ public class MainApp {
     public static void main(String[] args) throws Exception {
         TraceHookCar tracecar = new TraceHookCar("Chevy", "Black", 2008);
         System.out.println(tracecar.calculateCarAgeAndOilChanges());
+
+        JacksonBradleyCar jacksoncar = new JacksonBradleyCar("Ford", "Blue", 1989);
+        System.out.println(jacksoncar.oilChangeNeeded());
+
+        
     }
 }
