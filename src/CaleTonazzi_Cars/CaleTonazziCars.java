fde61ab0a119c11bc3403ce7b764303939b70736
ctonazzi <ctonazzi@gmail.com>
2024-09-06 09:06:12 -0500

    created car class

---
+++ src/CaleTonazzi_Cars/CaleTonazziCars.java
@@ -0,0 +1,37 @@
+package CaleTonazzi_Cars;
+
+public class CaleTonazziCars {
+    String carType;
+    String carColor;
+    int carYear;
+
+    public CaleTonazziCars(String carType, String carColor, int carYear) {
+        this.carType = carType;
+        this.carColor = carColor;
+        this.carYear = carYear;
+    }
+
+    public String getCarType() {
+        return carType;
+    }
+
+    public String getCarColor() {
+        return carColor;
+    }
+
+    public int getCarYear() {
+        return carYear;
+    }
+
+    public void setCarType(String carType) {
+        this.carType = carType;
+    }
+
+    public void setCarColor(String carColor) {
+        this.carColor = carColor;
+    }
+
+    public void setCarYear(int carYear) {
+        this.carYear = carYear;
+    }
+}
