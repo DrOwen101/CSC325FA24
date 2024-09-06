5e5b4f0cc6aff43df6834ef92810b0c27a2913ea
gtuckerjones <gjones28@murraystate.edu>
2024-09-06 09:40:51 -0500

    Merge remote-tracking branch 'origin/main' into gtuckerjonesBranch

--- src/AnimalsExercise/EthanFowler_Animal/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Cat cat = new Cat();
-        Dog dog = new Dog();
-        System.out.println(cat.toString());
-        System.out.println(dog.toString());
-        
-    }
-}
