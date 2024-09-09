94f819d864c068f47723cf0438fa8a7d1efbb585
gtuckerjones <gjones28@murraystate.edu>
2024-09-09 09:44:27 -0500

    Car app submission

--- src/as2_2/Assignment2/src/MainApp.java
+++ src/as2_2/Assignment2/src/MainApp.java
@@ -14,5 +14,8 @@ public class MainApp {
 
         EmiliaKubikCar emiliacar = new EmiliaKubikCar("Suzuki", "Green", 1999);
         System.out.println(emiliacar.carAge());
+
+        TuckerJonesCar tuckercar = new TuckerJonesCar("Honda", "Insight", "Grey", 2021);
+        System.out.println(tuckercar.oilChange());
     }
 }
