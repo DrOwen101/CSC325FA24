ee65d718852aa5460d730bfd145ad0e66c3e536e
JacksonCash <jackson.w.cash04@gmail.com>
2024-09-09 08:58:12 -0500

    Assignment 2.2

---
+++ src/as2_2/Assignment2/src/JacksonCashCar.java
@@ -0,0 +1,44 @@
+public class JacksonCashCar {
+    private String carType;
+    private String carColor;
+    private int carYear;
+    
+
+    public void setCarType(String newCarType) {
+        carType = newCarType;
+    }
+    public void setCarColor(String newCarColor) {
+        carColor = newCarColor;
+    }
+    public void setCarYear(int newCarYear) {
+        carYear = newCarYear;
+    }
+
+
+    public String getCarType() {
+        return carType;
+    }
+    public String getCarColor() {
+        return carColor;
+    }
+    public int getCarYear() {
+        return carYear;
+    }
+
+    public String getCarAge(int carYear) {
+        if(carYear > 2024) {
+            return "invalid Year entered. Please enter the year your car was made again.";
+        } else {
+            return "" + (2024 - carYear) + " year old car.";
+        }
+        
+    }
+    public int getOCCount(int carYear) {
+        int i;
+        for (i = 0; carYear > 0;) {
+            carYear = (2024 - carYear) - 3;
+            ++i;
+        }
+        return i;
+    }
+}
