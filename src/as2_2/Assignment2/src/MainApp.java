4f5d8339423340f3e25f7b444b97f950989baf58
Jason Owen <jasonowen101@gmail.com>
2024-09-09 13:25:41 -0500

    commit remove main merge conflict

--- src/as2_2/Assignment2/src/MainApp.java
+++ src/as2_2/Assignment2/src/MainApp.java
@@ -15,10 +15,9 @@ public class MainApp {
         EmiliaKubikCar emiliacar = new EmiliaKubikCar("Suzuki", "Green", 1999);
         System.out.println(emiliacar.carAge());
 
-<<<<<<< HEAD
         MarkSharovarovCar carMark = new MarkSharovarovCar("BMW", "Black", 2017);
         System.out.println(carMark.getCarAgeAndNumberOfOilChanges());
-=======
+
         TuckerJonesCar tuckercar = new TuckerJonesCar("Honda", "Insight", "Grey", 2021);
         System.out.println(tuckercar.oilChange());
         
@@ -27,6 +26,5 @@ public class MainApp {
 
         AndiaKaranjaCar andiacar = new AndiaKaranjaCar("Toyota", "gold", 1999);
         System.out.println(andiacar.carAgeAndOilChange());
->>>>>>> main
     }
 }
