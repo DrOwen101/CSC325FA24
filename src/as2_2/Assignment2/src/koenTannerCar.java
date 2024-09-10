ac8c0f360f904d87490b60065f883aa437bb3184
koen-tanner5 <koen.tanner5@gmail.com>
2024-09-09 23:43:17 -0500

    age and oil change method added

--- src/as2_2/Assignment2/src/koenTannerCar.java
+++ src/as2_2/Assignment2/src/koenTannerCar.java
@@ -33,4 +33,16 @@ public class koenTannerCar {
         this.carYear = carYear;
     }
 
+    public String ageAndOilChange() {
+        int carAge = 2024 - carYear;
+        int oilChanges = 0;
+
+        for (int i = 0; i < carAge; i++) {
+            if (i % 3 == 0) {
+                oilChanges++;
+            }
+        }
+        return "Car is " + carAge + " years old, and should have had " + oilChanges + " oil changes.";
+    }
+
 }
