6f4b9b6e12c45508e37f22edbe79949a3fa29e5d
Bwells23-murray <Bwells23@murraystate.edu>
2024-09-09 22:12:38 -0500

    car app

--- src/as2_2/Assignment2/src/MainApp.java
+++ src/as2_2/Assignment2/src/MainApp.java
@@ -14,5 +14,8 @@ public class MainApp {
 
         EmiliaKubikCar emiliacar = new EmiliaKubikCar("Suzuki", "Green", 1999);
         System.out.println(emiliacar.carAge());
+
+        BrooklynWellsCar brooklynCar = new BrooklynWellsCar("Acura", "Red", 1990);
+        System.out.println(brooklynCar.oilChange());
     }
 }
