9402d21962e9e97afa390360307c227901b1341d
MUS1C17 <msharovarov@murraystate.edu>
2024-09-15 23:14:17 -0500

    Finished Assignment

--- src/as2_2/Assignment2/src/MainApp.java
+++ src/as2_2/Assignment2/src/MainApp.java
@@ -11,6 +11,9 @@ public class MainApp {
 
         EthanSextonAS2d ethanSextonTest = new EthanSextonAS2d();
         System.out.println("Ethan Sexton - While Loop: " + ethanSextonTest.whileLoop() + "\nFor Loop: " + ethanSextonTest.forLoop());
+
+        MarkSharovarovAS2d testMark = new MarkSharovarovAS2d();
+        System.out.println("Mark Sharovarov - " + testMark.loopsMethod());
         
         // /*TraceHookCar tracecar = new TraceHookCar("Chevy", "Black", 2008);
         // System.out.println(tracecar.calculateCarAgeAndOilChanges());
