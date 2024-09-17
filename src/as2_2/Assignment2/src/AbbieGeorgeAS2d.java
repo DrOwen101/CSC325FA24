dedae8d0ca5b787b2b7d1fed3f1d5949ff9acd50
squirrelyCoding <georgeabbie0@gmail.com>
2024-09-16 19:33:12 -0500

    AG: while loop assignment completed, now with comments

--- src/as2_2/Assignment2/src/AbbieGeorgeAS2d.java
+++ src/as2_2/Assignment2/src/AbbieGeorgeAS2d.java
@@ -1,12 +1,15 @@
 public class AbbieGeorgeAS2d {
     
+    //creates a private instance of an array
     private int[] arr = {1,2,3,4};
 
+    //public method that holds the while and for loop, as well as returns results for both loops
     public String arrayMethod(){
         int count = 0;
         int index = 0;
         String message = "";
 
+        //while loop that breaks out after index number three is read
         while (index < arr.length){
             count++;
             if (index == 3){
@@ -15,6 +18,7 @@ public class AbbieGeorgeAS2d {
             index++;
         }
         
+        //a for loop that increments i to 100 and then outputs "I counted to 100" when it's reached 100
         for (int i = 1; i <= 100; i++){
             if (i == 100) {
                 message = "I counted to 100";
@@ -22,8 +26,8 @@ public class AbbieGeorgeAS2d {
             
 
         }
-
+        //returns the output for both the while and for loop
         return "Number of iterations: " + count + ". " + message;
     }
 
-}
+} //close class
